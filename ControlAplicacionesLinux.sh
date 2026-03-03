e685c1e1af078c05e5b5878460120fe147385641
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-03-03 19:22:32 +0100

    Update ControlAplicacionesLinux.sh

--- ControlAplicacionesLinux.sh
+++ ControlAplicacionesLinux.sh
@@ -354,7 +354,7 @@ check_root
 
 zenity --info --title="Estado" --text="Zenity correcto" --timeout=0.1 > /dev/null 2>&1
 #zenity --info --title="Estado" --text="Zenity correcto" > /dev/null 2>&1
-if [ ! $? = 255 ]; then
+if [ ! $? = 0 ]; then
     echo -e ""
     echo -e "${rojo} Fallo en la ejecucion de entorno grafico.${borra_colores} Posibles causas:"
     echo -e ""
